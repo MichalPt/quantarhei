9eb19f0951cd4f375b8604132b3eb945b313aafe
MichalPt <michalpt@users.noreply.github.com>
2020-03-17 19:21:51 +0100

    Few edits

--- quantarhei/qm/propagators/plot_colour_and_legend_manager.py
+++ quantarhei/qm/propagators/plot_colour_and_legend_manager.py
@@ -1,18 +1,19 @@
 import numpy as np
-from operator import itemgetter
 from collections import Counter
 import matplotlib.pylab as plt
 
+'''
+ColLegManager function inputs are:
+      >>> colours   <dict> - dict connecting molecule types (e.g. Chla --> Chla1, Chla2, ...)
+                             with color maps of user's choice (e.g. 'Reds', 'Blues', ...)
+      >>> legend    <list> - list of molecules names as they are present in density matrix (in correct order)
+      >>> N         <int>  - dimension of density matrix (usually one element longer than legend length
 
-# ColLegManager function inputs are:
-#       >>> colours   <dict> - dict connecting molecule types (e.g. Chla --> Chla1, Chla2, ...)
-#                              with color maps of user's choice (e.g. 'Reds', 'Blues', ...)
-#       >>> legend    <list> - list of molecules names as they are present in density matrix (in correct order)
-#       >>> N         <int>  - dimension of density matrix (usually one element longer than legend length
-#
-# ColLegManager unction returns list of:
-#  [0]  >>> order     <list> - list of molecule names' indexes in 'legend' (input) in alphabetical order
-#  [1]  >>> paintings <list> - list of RGBA vectors for each molecule name, order of values corresponds to the legend
+ColLegManager unction returns list of:
+ [0]  >>> order     <list> - list of molecule names' indexes in 'legend' (input) in alphabetical order
+ [1]  >>> paintings <list> - list of RGBA vectors for each molecule name, order of values corresponds to the legend
+'''
+
 
 def ColLegManager(colours=None, legend=None, N=None):
     # If no dict of colours_map or legend or N has been entered
@@ -23,18 +24,27 @@ def ColLegManager(colours=None, legend=None, N=None):
     if not N:
         N = len(legend) + 1
 
-    #Constructing list of indexes for alphabetically ordered molecule names in 'legend'
-    if legend != None:
+    # Constructing list of indexes for alphabetically ordered molecule names in 'legend'
+    if legend is not None:
         order = np.argsort(legend)
         print("Order_of_mol_names: ", list(order))
 
+    def ColMatcher(*names):
+        for name in names:
+            if mol.find(name) != -1:
+                cols.append(key)
+        return cols
+
     cols = []
-    if colours != None:
+    if colours is not None:
         # Sorting molecules by colours:
         for mol in legend:
             for key in list(colours.keys()):
-                if mol.find(colours[key]) != -1:
-                    cols.append(key)
+                cmap = colours[key]
+                if isinstance(cmap, (list, tuple)):
+                    ColMatcher(*cmap)
+                else:
+                    ColMatcher(cmap)
 
         print("Input_legend:       ", legend)
         print("Matched_col_maps:   ", cols)
@@ -55,9 +65,8 @@ def ColLegManager(colours=None, legend=None, N=None):
                 paintings[loc] = plt.get_cmap(clr)(1 - gradient[i])
             q = q + numocc
 
-
     # Default setting - gradient of colours of preset colour map
-    if colours == None:
+    if colours is None:
         paintings = [0] * N
         gradient = np.linspace(0.2, 1, N)
         j = 0
